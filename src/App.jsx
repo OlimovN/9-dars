88fb3854163aba72c560d38c27d97e0e2386b076
OlimovN <olimonurmuhammad711@gmail.com>
2024-08-30 14:54:23 +0500

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
 import { Routes,Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import Bookmarked from './pages/Bookmarked'
@@ -12,8 +10,6 @@ function App() {
        <Routes>
         <Route path="/" element={<MainLayout><Home /></MainLayout>} />
         <Route path="/bookmarked" element={<MainLayout><Bookmarked /></MainLayout>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
